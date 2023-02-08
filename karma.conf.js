fe4390c07ea40ec0df0d2cb177c808a50107c1ae
Romain DELEAU <romain.deleau@imt-nord-europe.fr>
2023-02-08 10:53:16 +0100

    change application name to RLG Maker

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/rlgtool'),
+      dir: require('path').join(__dirname, './coverage/rlgmaker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
